38e4dcb8885122c8e4a69bc3ab1c0b7e7e6a3b4c
Yuvraj686 <yuvigarg6806@gmail.com>
2025-10-20 17:24:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask, render_template 
+from flask import Flask,render templates
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == '__main__':
+
+    app.run()
